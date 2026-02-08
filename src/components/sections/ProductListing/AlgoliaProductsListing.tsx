ab17363431ad4757d7818a1f908a671e763087eb
kunstogutstyr <post@kunstogutstyr.no>
2026-02-08 13:36:55 +0100

    Fjernet variants price amount fra algolia filter

--- src/components/sections/ProductListing/AlgoliaProductsListing.tsx
+++ src/components/sections/ProductListing/AlgoliaProductsListing.tsx
@@ -45,7 +45,7 @@ export const AlgoliaProductsListing = ({
     seller_handle
       ? `NOT seller:null AND seller.handle:${seller_handle} AND `
       : "NOT seller:null AND "
-  }NOT seller.store_status:SUSPENDED AND supported_countries:${locale} AND variants.prices.currency_code:${currency_code} AND variants.prices.amount > 0${
+  }NOT seller.store_status:SUSPENDED AND supported_countries:${locale} AND variants.prices.currency_code:${currency_code}0${
     category_id
       ? ` AND categories.id:${category_id}${
           collection_id !== undefined
